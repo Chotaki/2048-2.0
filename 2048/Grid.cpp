c5eaabf47bdba0a848206598e56e0eac9d5b8b3e
bturcat <bturcat@GCFRLY238>
2023-10-26 12:56:22 +0200

    play

--- 2048/Grid.cpp
+++ 2048/Grid.cpp
@@ -70,88 +70,92 @@ void Grid::Tileplay(string x)
 {
     int temp;
     int pos;
-    for (int i = 0; i < iGridSize / 4;i++) 
+    if (x == "droite" or x == "bas")
     {
-        for (int j = 0; j < iGridSize / 4;j++) 
+        for (int i = 0; i < iGridSize / 4; i++)
         {
-            pos = (Position(i, j));
-            temp = grid[pos].GetValues();
+            for (int j = 0; j < iGridSize / 4; j++)
+            {
 
-            // glisement droite
-            if (x == "droite")
-            {
-                if (temp != 0)
+                pos = (Position(i, j));
+                temp = grid[pos].GetValues();
+
+                // glisement droite
+                if (x == "droite")
                 {
-                    if (pos - 1 < i * 4)
+                    if (temp != 0)
                     {
-                        cout << "out of range" << endl;
-                    }
-                    else {
-                        if (temp == grid[pos - 1].GetValues())
+                        if (pos - 1 < i * 4)
                         {
-                            
-                            if (pos - 1 >= 0) {
-                                grid[pos].ChangeValue(temp * 2);
-                                grid[pos - 1].ChangeValue(0);
-                            }
-                            
+                            cout << "out of range" << endl;
                         }
-                    }
-                    
-                }
-                else 
-                {
-                    if (pos-1 < i * 4) 
-                    {
-                        cout << "out of RANGE" << endl;
-                    }
-                    else 
-                    {
-                        if (pos - 1 >= 0) {
-                            if (grid[pos - 1].GetValues() != 0)
+                        else {
+                            if (temp == grid[pos - 1].GetValues())
                             {
-                                grid[pos].ChangeValue(grid[pos - 1].GetValues());
-                                grid[pos - 1].ChangeValue(0);
+
+                                if (pos - 1 >= 0) {
+                                    grid[pos].ChangeValue(temp * 2);
+                                    grid[pos - 1].ChangeValue(0);
+                                }
+
                             }
                         }
-                    }
-                }
-            }
-            //glissement gauche
-            else if (x == "gauche")
-            {
-                if (temp != 0)
-                {
-                    if (pos + 1 >= (i + 1) * 4)
-                    {
-                        cout << "out of range2" << endl;
+
                     }
                     else
                     {
-                        if (temp == grid[pos + 1].GetValues())
+                        if (pos - 1 < i * 4)
                         {
-                            if (pos + 1 <= 15) 
-                            {
-                                grid[pos].ChangeValue(temp * 2);
-                                grid[pos + 1].ChangeValue(0);
+                            cout << "out of RANGE" << endl;
+                        }
+                        else
+                        {
+                            if (pos - 1 >= 0) {
+                                if (grid[pos - 1].GetValues() != 0)
+                                {
+                                    grid[pos].ChangeValue(grid[pos - 1].GetValues());
+                                    grid[pos - 1].ChangeValue(0);
+                                }
                             }
                         }
                     }
-
                 }
-                else
+                //glissement bas
+                else if (x == "bas")
                 {
-                    if (pos + 1 >= (i + 1) * 4) 
+                    if (temp != 0)
                     {
-                        cout << "out of RANGE2" << endl;
-                    }
-                    else 
-                    {
-                        if (pos + 1 <= 15) {
-                            if (grid[pos + 1].GetValues() != 0)
+                        if (pos - 4 < 0 )
+                        {
+                            cout << "out of range" << endl;
+                        }
+                        else {
+                            if (temp == grid[pos - 4].GetValues())
                             {
-                                grid[pos].ChangeValue(grid[pos + 1].GetValues());
-                                grid[pos + 1].ChangeValue(0);
+
+                                if (pos + 4 >= 0) {
+                                    grid[pos].ChangeValue(temp * 2);
+                                    grid[pos - 4].ChangeValue(0);
+                                }
+
+                            }
+                        }
+
+                    }
+                    else
+                    {
+                        if (pos - 4 < 0)
+                        {
+                            cout << "out of RANGE" << endl;
+                        }
+                        else
+                        {
+                            if (pos - 4 >= 0) {
+                                if (grid[pos - 4].GetValues() != 0)
+                                {
+                                    grid[pos].ChangeValue(grid[pos - 4].GetValues());
+                                    grid[pos - 4].ChangeValue(0);
+                                }
                             }
                         }
                     }
@@ -159,4 +163,96 @@ void Grid::Tileplay(string x)
             }
         }
     }
+            
+    else if (x == "gauche" or x == "haut")
+    {
+        for(int k = 3;k >= 0; k--)
+        {
+            for(int l = 3; l>=0 ; l--)
+            {
+                pos = (Position(k, l));
+                temp = grid[pos].GetValues();
+                //glissement gauche
+                if (x == "gauche")
+                {
+                    if (temp != 0)
+                    {
+                        if (pos + 1 >= (k+1)  * 4)
+                        {
+                            cout << "out of range2" << endl;
+                        }
+                        else
+                        {
+                            if (temp == grid[pos + 1].GetValues())
+                            {
+                                if (pos + 1 <= 15)
+                                {
+                                    grid[pos].ChangeValue(temp * 2);
+                                    grid[pos + 1].ChangeValue(0);
+                                }
+                            }
+                        }
+
+                    }
+                    else
+                    {
+                        if (pos + 1 >= (k+1) * 4)
+                        {
+                            cout << "out of RANGE2" << endl;
+                        }
+                        else
+                        {
+                            if (pos + 1 <= 15) {
+                                if (grid[pos + 1].GetValues() != 0)
+                                {
+                                    grid[pos].ChangeValue(grid[pos + 1].GetValues());
+                                    grid[pos + 1].ChangeValue(0);
+                                }
+                            }
+                        }
+                    }
+                }
+                else if (x == "haut")
+                {
+                    if (temp != 0)
+                    {
+                        if (pos + 4 >15)
+                        {
+                            cout << "out of range2" << endl;
+                        }
+                        else
+                        {
+                            if (temp == grid[pos + 4].GetValues())
+                            {
+                                if (pos + 4 <= 15)
+                                {
+                                    grid[pos].ChangeValue(temp * 2);
+                                    grid[pos + 4].ChangeValue(0);
+                                }
+                            }
+                        }
+
+                    }
+                    else
+                    {
+                        if (pos + 4 > 15)
+                        {
+                            cout << "out of RANGE2" << endl;
+                        }
+                        else
+                        {
+                            if (pos + 4 <= 15) {
+                                if (grid[pos + 4].GetValues() != 0)
+                                {
+                                    grid[pos].ChangeValue(grid[pos + 4].GetValues());
+                                    grid[pos + 4].ChangeValue(0);
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+            
+        }
+    }   
 }
